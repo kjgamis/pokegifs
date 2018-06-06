f1f80d58a8354bbea7747c165d9c14b9ba69501b
Karen <kjgamis@live.ca>
2018-06-06 00:31:56 -0400

    change instance variables

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -6,24 +6,24 @@ class PokemonController < ApplicationController
     pokemon_response = HTTParty.get("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json")
     pokemon_body = JSON.parse(pokemon_response.body)
 
-    @id   = pokemon_body["pokemon"][0]["id"]
-    @name = pokemon_body["pokemon"][0]["name"]
-    @type = pokemon_body["pokemon"][0]["type"]
+    @pokemon_id   = pokemon_body["pokemon"][0]["id"]
+    @pokemon_name = pokemon_body["pokemon"][0]["name"]
+    @pokemon_type = pokemon_body["pokemon"][0]["type"]
 
 
     gif_response = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{key}&q=bulbasaur&rating=g")
     gif_body = JSON.parse(gif_response.body)
 
-    @gif = gif_body["data"][0]["images"]["original"]["url"]
+    @pokemon_gif = gif_body["data"][0]["images"]["original"]["url"]
 
     respond_to do |format|
       format.html { render :index }
 
       format.json { render json: {
-        id:    @id,
-        name:  @name,
-        type:  @type,
-        gif:   @gif
+        id:    @pokemon_id,
+        name:  @pokemon_name,
+        type:  @pokemon_type,
+        gif:   @pokemon_gif
       } }
     end
 
