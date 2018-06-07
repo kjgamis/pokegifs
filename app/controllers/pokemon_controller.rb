c8df69c2a21f99523a305569ff09605cb86719bc
Karen <kjgamis@live.ca>
2018-06-07 16:28:57 -0400

    add other api endpoints

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -6,13 +6,17 @@ class PokemonController < ApplicationController
     pokemon_response = HTTParty.get("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json")
     pokemon_body = JSON.parse(pokemon_response.body)
 
-    @pokemon_id     = pokemon_body["pokemon"][0]["id"]
-    @pokemon_name   = pokemon_body["pokemon"][0]["name"]
-    @pokemon_type   = pokemon_body["pokemon"][0]["type"]
-    @pokemon_height = pokemon_body["pokemon"][0]["height"]
-    @pokemon_weight = pokemon_body["pokemon"][0]["weight"]
-    @pokemon_candy  = pokemon_body["pokemon"][0]["candy"]
+    @pokemon_id           = pokemon_body["pokemon"][0]["id"]
+    @pokemon_name         = pokemon_body["pokemon"][0]["name"]
+    @pokemon_type         = pokemon_body["pokemon"][0]["type"]
+    @pokemon_height       = pokemon_body["pokemon"][0]["height"]
+    @pokemon_weight       = pokemon_body["pokemon"][0]["weight"]
+    @pokemon_candy        = pokemon_body["pokemon"][0]["candy"]
     @pokemon_spawn_chance = pokemon_body["pokemon"][0]["spawn_chance"]
+    @pokemon_avg_spawns   = pokemon_body["pokemon"][0]["avg_spawns"]
+    @pokemon_spawn_time   = pokemon_body["pokemon"][0]["spawn_time"]
+    @pokemon_multipliers  = pokemon_body["pokemon"][0]["multipliers"]
+    @pokemon_weaknesses   =pokemon_body["pokemon"][0]["weaknesses"]
 
 
     gif_response = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{key}&q=#{@pokemon_name}&rating=g")
@@ -51,15 +55,23 @@ class PokemonController < ApplicationController
         @pokemon_weight = pokemon_body["pokemon"][0]["weight"]
         @pokemon_candy  = pokemon_body["pokemon"][0]["candy"]
         @pokemon_spawn_chance = pokemon_body["pokemon"][0]["spawn_chance"]
+        @pokemon_avg_spawns   = pokemon_body["pokemon"][0]["avg_spawns"]
+        @pokemon_spawn_time   = pokemon_body["pokemon"][0]["spawn_time"]
+        @pokemon_multipliers  = pokemon_body["pokemon"][0]["multipliers"]
+        @pokemon_weaknesses   = pokemon_body["pokemon"][0]["weaknesses"]
       elsif input.to_i == 0
         if input.capitalize == pokemon["name"]
-          @pokemon_id   = pokemon["id"]
+          @pokemon_id           = pokemon["id"]
           @pokemon_name = pokemon["name"]
           @pokemon_type = pokemon["type"]
           @pokemon_height = pokemon_body["pokemon"][0]["height"]
           @pokemon_weight = pokemon_body["pokemon"][0]["weight"]
           @pokemon_candy  = pokemon_body["pokemon"][0]["candy"]
           @pokemon_spawn_chance = pokemon_body["pokemon"][0]["spawn_chance"]
+          @pokemon_avg_spawns   = pokemon_body["pokemon"][0]["avg_spawns"]
+          @pokemon_spawn_time   = pokemon_body["pokemon"][0]["spawn_time"]
+          @pokemon_multipliers  = pokemon_body["pokemon"][0]["multipliers"]
+          @pokemon_weaknesses   = pokemon_body["pokemon"][0]["weaknesses"]
         end
       end
     end
